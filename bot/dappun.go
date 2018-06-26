00cb76abd98be028585c2fae1bbab0d852c73c51
silverscat <hatakugo@outlook.jp>
2018-06-26 13:13:24 +0000

    脱糞のバリエーションを追加

--- bot/dappun.go
+++ bot/dappun.go
@@ -1,12 +1,25 @@
 package bot
 
-import "github.com/nlopes/slack"
+import (
+	"math/rand"
+	"time"
+
+	"github.com/nlopes/slack"
+)
 
 func (b *Bot) handleDappun(ev *slack.MessageEvent, args ...string) error {
-	butimili := "*うおおおおおおおおおおおおあああああああああああああああああああああああああああああああ！！！！！！！！！！！ (ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ！！！！！！！)*"
+	butimili := []string{
+		"*あああああああああああああああああああああああああああああああ!!!!!!!!!!!(ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ!!!!!!ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ!!!!!!!)*",
+		"*(ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ!!!!!!ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ!!!!!!!)あああああああああああああああああああああああああああああああ!!!!!!!!!!!*",
+		"*are are are are are are are are are!!!!!!!!!!!!!(Boo lee Boo lee Boo lee Boo Liu Liu Liu Liu Liu Liu !!!!!!!!!!!! Boo tea tea Boo Boo Boo tea tea tea tea Boo lee lee Boo Boo Boo Boo woo woo woo uh)*",
+		"*うおおおおおおおおおおおおああああああああああああああああああ!!!!!!!!!!!(ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ!!!!!!ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ!!!!!!!)*",
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
 	attachment := slack.Attachment{
 		Color: "#994C00",
-		Text:  butimili,
+		Text:  butimili[rand.Intn(len(butimili)-1)],
 	}
 	params := slack.PostMessageParameters{}
 	params.Attachments = []slack.Attachment{attachment}
